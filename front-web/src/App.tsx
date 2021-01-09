dc1abb94a0a492ee8375da91387a7d0563343f5f
unknown <eliaslima900@hotmail.com>
2021-01-09 01:41:08 -0300

    add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import Navbar from './Navbar/index'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
